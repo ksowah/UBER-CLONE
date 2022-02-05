0c73dbee9fb98a0c331548bf2f4cc621ac09d633
ksowah <sowahkelvin640@gmail.com>
2022-02-05 23:34:46 +0000

    update

--- pages/confirm.js
+++ pages/confirm.js
@@ -64,6 +64,7 @@ const confirm = () => {
                     width: '100vw'
                 }}
                  fitBounds={[initialDestination, finalDestination]}
+                 
                 >
                 <Layer type="symbol" id="marker"  layout={{ 'icon-image': 'marker-15' }}>
                     <Feature coordinates={[-0.481747846041145, 51.3233379650232]} />
