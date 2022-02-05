59a3711ff0e0b0c4f256ae9f492d9297c7beb2cf
ksowah <sowahkelvin640@gmail.com>
2022-02-05 13:00:16 +0000

    update

--- pages/confirm.js
+++ pages/confirm.js
@@ -1,5 +1,6 @@
 import ReactMapboxGl, { Layer, Feature, Marker } from 'react-mapbox-gl';
 import Vehicles from '../components/Vehicles';
+import Head from 'next/head'
 import {useRouter} from 'next/router'
 import { useEffect, useState } from 'react';
 
@@ -20,12 +21,14 @@ const confirm = () => {
     const [initialDestination, setInitialDestination] = useState([])
     const [finalDestination, setFinalDestination] = useState()
 
+    setInitialDestination([2, 4])
+
     const getPickUp = (pickup) => {
          fetch(`https://api.mapbox.com/geocoding/v5/mapbox.places/${pickup}.json?types=place%2Cpostcode%2Caddress&access_token=${token}`)
          .then ((res) => res.json()) 
          .then ((data) => {
-            setInitialDestination(data.features[0].center)
-            console.log(initialDestination);
+            // setInitialDestination(data.features[0].center)
+            // console.log(initialDestination);
          })
         
     }
@@ -33,8 +36,8 @@ const confirm = () => {
     const getDropoff = async (dropoff)=> {
         const request = await fetch(`https://api.mapbox.com/geocoding/v5/mapbox.places/${dropoff}.json?types=place%2Cpostcode%2Caddress&access_token=${token}`)
         const res = await request.json() 
-        setFinalDestination(res?.features[0]?.center)
-        console.log(finalDestination);
+        // setFinalDestination(res?.features[0]?.center)
+        // console.log(finalDestination);
     }
        
 
@@ -44,55 +47,62 @@ const confirm = () => {
     }, [pickup, dropoff])
 
   return (
-  <div className="">
-        <div className='flex flex-col'> 
-            <Map
-            style="mapbox://styles/mapbox/streets-v9"
-            containerStyle={{
-                height: '60vh',
-                width: '100vw'
-            }}
-            // fitBounds={[initialDestination, finalDestination]}
-            >
-            <Layer type="symbol" id="marker"  layout={{ 'icon-image': 'marker-15' }}>
-                <Feature coordinates={[-0.481747846041145, 51.3233379650232]} />
-            </Layer>
 
-            {/* <Marker
-                coordinates={initialDestination}
-                anchor="bottom">
-                <img src={'https://th.bing.com/th/id/R.6d7cede615c35d03034dbd85a1428382?rik=eOSNgMXl2ujVGw&pid=ImgRaw&r=0'}
-                className='h-8 animate-bounce'
-                />
-            </Marker>
-            <Marker
-                coordinates={finalDestination}
-                anchor="bottom">
-                <img src={'https://th.bing.com/th/id/R.6d7cede615c35d03034dbd85a1428382?rik=eOSNgMXl2ujVGw&pid=ImgRaw&r=0'}
-                className='h-8 animate-bounce'
-                />
-            </Marker> */}
-            </Map>
-
-         </div>
-
-         <div className='flex flex-col'>
-            <div className='text-center w-full text-gray-400 p-2'>
-                choose a ride or swipe up for more
-            </div> 
-
-            <div className='flex flex-col border-t-2 border-b-2 h-80 overflow-auto scrollbar-hide'>
-                <Vehicles/>
-                <Vehicles/>
-                <Vehicles/>
-                <Vehicles/>
-            </div>
-         </div>
-
-         <button className='w-full bg-black active:bg-gray-900 p-2 text-xl m-4 text-white scroll'>
-             Confirm UberX 
-         </button>
-  </div>
+    <>
+         <Head>
+            <title>Confirm Ride</title>
+        </Head>
+    
+      <div className="">
+            <div className='flex flex-col'> 
+                <Map
+                style="mapbox://styles/mapbox/streets-v9"
+                containerStyle={{
+                    height: '60vh',
+                    width: '100vw'
+                }}
+                // fitBounds={[initialDestination, finalDestination]}
+                >
+                <Layer type="symbol" id="marker"  layout={{ 'icon-image': 'marker-15' }}>
+                    <Feature coordinates={[-0.481747846041145, 51.3233379650232]} />
+                </Layer>
+    
+                {/* <Marker
+                    coordinates={initialDestination}
+                    anchor="bottom">
+                    <img src={'https://th.bing.com/th/id/R.6d7cede615c35d03034dbd85a1428382?rik=eOSNgMXl2ujVGw&pid=ImgRaw&r=0'}
+                    className='h-8 animate-bounce'
+                    />
+                </Marker>
+                <Marker
+                    coordinates={finalDestination}
+                    anchor="bottom">
+                    <img src={'https://th.bing.com/th/id/R.6d7cede615c35d03034dbd85a1428382?rik=eOSNgMXl2ujVGw&pid=ImgRaw&r=0'}
+                    className='h-8 animate-bounce'
+                    />
+                </Marker> */}
+                </Map>
+    
+             </div>
+    
+             <div className='flex flex-col'>
+                <div className='text-center w-full text-gray-400 p-2'>
+                    choose a ride or swipe up for more
+                </div> 
+    
+                <div className='flex flex-col border-t-2 border-b-2 h-80 overflow-auto scrollbar-hide'>
+                    <Vehicles/>
+                    <Vehicles/>
+                    <Vehicles/>
+                    <Vehicles/>
+                </div>
+             </div>
+    
+             <button className='w-full bg-black active:bg-gray-900 p-2 text-xl m-4 text-white scroll'>
+                 Confirm UberX 
+             </button>
+      </div>
+    </>
   );
 };
 
