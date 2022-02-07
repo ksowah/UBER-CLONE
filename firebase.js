c279888f576f1c63539b87e45a5b603e7a042ee0
ksowah <sowahkelvin640@gmail.com>
2022-02-07 17:11:59 +0000

    updated new file

---
+++ firebase.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAXpmgzggkBQMLdWY0i0cx8GTZ7udBDEfY",
+  authDomain: "uber-425a4.firebaseapp.com",
+  projectId: "uber-425a4",
+  storageBucket: "uber-425a4.appspot.com",
+  messagingSenderId: "800955603427",
+  appId: "1:800955603427:web:52293973972cd7a64ea8d6",
+  measurementId: "G-RNK8XNCN66"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
