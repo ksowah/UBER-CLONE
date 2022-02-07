6c54aa5b2758546d8ae812658e60747ec9d9778a
ksowah <sowahkelvin640@gmail.com>
2022-02-07 17:32:44 +0000

    updated

--- firebase.js
+++ firebase.js
@@ -12,5 +12,7 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-export const app = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+
+export default app
