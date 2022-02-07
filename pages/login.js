dcabd6d605271e3ed690ae1f79991a8436b110e5
ksowah <sowahkelvin640@gmail.com>
2022-02-07 18:00:47 +0000

    update

--- pages/login.js
+++ pages/login.js
@@ -51,7 +51,7 @@ const login = () => {
                 </div>
             </div>
            
-        <button className="w-full py-2 mt-4 bg-black text-white text-lg" onClick={signIn()}>
+        <button className="w-full py-2 mt-4 bg-black text-white text-lg" onClick={()=>signInWithPopup(auth, app)}>
             Sign In With Google
         </button>
     </div>
