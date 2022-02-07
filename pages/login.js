01027caedca9250a5a4183ab0961b74b890febcc
ksowah <sowahkelvin640@gmail.com>
2022-02-07 16:35:26 +0000

    update

--- pages/login.js
+++ pages/login.js
@@ -9,17 +9,28 @@ const login = () => {
             <title>Login</title>
         </Head>
 
-    <div className="h-screen w-screen bg-gray-200 p-4">
+    <div className="h-screen bg-gray-200 p-4 w-auto">
            <div>
               <Image
-                src={'https://th.bing.com/th/id/R.ee430489d1505483166c19ab9ed00d4e?rik=9h%2bv%2fqv8kXnGKQ&pid=ImgRaw&r=0'}
-                height={30}
+                src={' https://i.ibb.co/n6LWQM4/Post.png'}
+                height={35}
                 width={100}
               />
             </div>
-            <div className="text-5xl pt-4 text-gray-500">
-                <h1>Log in to access your account</h1>
+
+            <div className="max-w-full  lg:max-w-2xl">
+                <div className="text-5xl pt-4 mr-4 mt-4 text-gray-500">
+                    <h1>Log in to access your account</h1>
+                </div>
+                
+                <div className='w-full object-contain px-2'>
+                    <img 
+                        src="https://i.ibb.co/CsV9RYZ/login-image.png"
+                        className=" object-contain"
+                    />
+                </div>
             </div>
+           
         <button className="w-full py-2 mt-4 bg-black text-white text-lg">
             Sign In With Google
         </button>
