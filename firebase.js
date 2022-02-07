2b66244c9379fd926ed72fb44cbe3d64b73b0826
ksowah <sowahkelvin640@gmail.com>
2022-02-07 17:35:58 +0000

    bug fix

--- firebase.js
+++ firebase.js
@@ -12,7 +12,5 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+export const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
-
-export default app
